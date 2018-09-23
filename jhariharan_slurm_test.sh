c522967aae56730852557439bf66b10da0f46689
jay <el_bee_jay@yahoo.com>
2018-09-23 15:33:21 -0500

    test03
    test03

--- jhariharan_slurm_test.sh
+++ jhariharan_slurm_test.sh
@@ -2,8 +2,7 @@
 
 #SBATCH --nodes=1
 #SBATCH --time=0:05:00
-#SBATCH --qos=testing
-#SBATCH --partition=shas-testing
+#SBATCH --qos=preemptable
 #SBATCH --ntasks=1
 #SBATCH --job-name=jhariharan-test
 #SBATCH --output=jhariharan-test.%j.out
